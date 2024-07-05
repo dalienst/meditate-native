57366879478c5fbfe8e0cfe61038717348d16267
dalienst <oduordalienst@gmail.com>
2024-07-05 12:37:00 +0300

    add splash logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,34 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
+import React from 'react'
+import {LinearGradient} from 'expo-linear-gradient'
+import beachImage from "@/assets/meditation-images/beach.webp";
+import {StatusBar} from 'expo-status-bar'
+import CustomButton from '@/components/CustomButton';
 
-export default function HomeScreen() {
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home screen</Text>
+    <View className='flex-1'>
+      <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
+        <LinearGradient className='flex-1' colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+
+          <SafeAreaView className='flex-1 mx-5 my-16 justify-between'>
+            <View>
+              <Text className='text-white text-center font-bold text-4xl'>Easy Meditation</Text>
+              <Text className='text-center text-white text-regular text-2xl mt-3'>Meditation made easy</Text>
+            </View>
+
+            <View>
+              <CustomButton onPress={() => alert('hello')} title="Get Started" />
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>
+
+        </LinearGradient>
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
+export default App
