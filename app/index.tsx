689a1252ad61cd4f930f77498cddeeabad402aff
dalienst <oduordalienst@gmail.com>
2024-07-05 13:45:21 +0300

    display images

--- app/index.tsx
+++ app/index.tsx
@@ -4,28 +4,27 @@ import {LinearGradient} from 'expo-linear-gradient'
 import beachImage from "@/assets/meditation-images/beach.webp";
 import {StatusBar} from 'expo-status-bar'
 import CustomButton from '@/components/CustomButton';
+import { useRouter } from 'expo-router';
+import AppGradient from '@/components/AppGradient';
 
 
 const App = () => {
+  const router = useRouter()
   return (
     <View className='flex-1'>
       <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
-        <LinearGradient className='flex-1' colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
-
-          <SafeAreaView className='flex-1 mx-5 my-16 justify-between'>
-            <View>
-              <Text className='text-white text-center font-bold text-4xl'>Easy Meditation</Text>
-              <Text className='text-center text-white text-regular text-2xl mt-3'>Meditation made easy</Text>
-            </View>
-
-            <View>
-              <CustomButton onPress={() => alert('hello')} title="Get Started" />
-            </View>
-
-            <StatusBar style="light" />
-          </SafeAreaView>
-
-        </LinearGradient>
+        <AppGradient colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+          <SafeAreaView className='flex-1 px-1 justify-between'>
+              <View>
+                <Text className='text-white text-center font-bold text-4xl'>Easy Meditation</Text>
+                <Text className='text-center text-white text-regular text-2xl mt-3'>Meditation made easy</Text>
+              </View>
+              <View>
+                <CustomButton onPress={() => router.push("/nature-meditate")} title="Get Started" />
+              </View>
+              <StatusBar style="light" />
+            </SafeAreaView>
+        </AppGradient>
       </ImageBackground>
     </View>
   )
